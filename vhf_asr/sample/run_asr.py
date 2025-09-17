543966497888c40d3960fa868ded92d170a4b3ff
Yaguchan <guchi4867@gmail.com>
2025-09-17 10:28:35 +0900

    20250917

--- vhf_asr/sample/run_asr.py
+++ vhf_asr/sample/run_asr.py
@@ -4,6 +4,19 @@ import subprocess
 
 
 # python run_asr.py
+NAME = '20250714mini_va'
+mp4_file = f'audio/{NAME}.mp4'
+output_file = f'text/{NAME}.txt'
+WORDPATH = 'wordlist/vhf.txt'
+
+
+def make_prompt(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        custom_words = [line.strip() for line in f if line.strip()]
+    initial_prompt = "この会話には " + "、".join(custom_words) + " などの固有名詞が出てきます。"
+    return initial_prompt
+
+
 def mp4_to_wav(mp4_path, wav_path):
     subprocess.call(['ffmpeg', '-i', mp4_path, '-ar', '16000', '-ac', '1', wav_path])
 
@@ -15,22 +28,30 @@ def format_timestamp(seconds):
     return f"{minutes:02d}:{secs:02d}"
 
 
-def transcribe_with_whisper(mp4_path):
-    temp_wav = "temp.wav"
+def transcribe_with_whisper(mp4_path, options):
+    temp_wav = "audio/temp.wav"
     mp4_to_wav(mp4_path, temp_wav)
-
     model = whisper.load_model("medium")  # 必要に応じて small / large に変更可
-    result = model.transcribe(temp_wav, fp16=False)
-
+    result = model.transcribe(
+        temp_wav,
+        **options
+    )
     os.remove(temp_wav)
     return result["segments"]  # 各セグメントに start, end, text が含まれる
 
 
 def main():
-    mp4_file = "audio/20250714mini_va.mp4"
-    segments = transcribe_with_whisper(mp4_file)
-
-    output_file = "transcribed_text_with_timestamps.txt"
+    
+    initial_prompt = make_prompt(WORDPATH)
+    options = {
+        "language": "ja", 
+        "initial_prompt": initial_prompt,
+        "task": "transcribe",
+        "fp16": False
+    }
+    print(options)
+    
+    segments = transcribe_with_whisper(mp4_file, options)
     with open(output_file, "w", encoding="utf-8") as f:
         for seg in segments:
             start = format_timestamp(seg["start"])
